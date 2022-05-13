7f39f850d67385898ef212566a753fc160c60f99
Kai Yi (David) Zhang <61890246+KaiYi-David-Zhang@users.noreply.github.com>
2022-05-13 17:01:06 -0400

    - finished implementation of encryption and decryption in CBC mode - added add, list, display functionalities

--- ImageRepo.java
+++ ImageRepo.java
@@ -1,39 +1,90 @@
 import java.awt.*;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 
 public class ImageRepo {
-    static int key = 11111111;
-    int IV = 1;
+    static int key = 1241085523;
+    static int IV = -1826778;
+    static String database = "database";
+    static String display = "display";
+
     public static void main(String[] args) {
         String command = args[0];
+        /*
         if(command.equals("-encrypt") || command.equals("-en")){
-            encrypt(args[1]);
+            encryptFile(args[1]);
         }
         else if(command.equals("-decrypt") || command.equals("-de")){
-            decrypt(args[1]);
+            decryptFile(args[1]);
         }
-
-
+        */
+        if(command.equals("-add")){
+            addFile(args[1]);
+        }
+        else if(command.equals("-list")){
+            listFile();
+        }
+        else if(command.equals("-display")){
+            displayFile(args[1]);
+        }
 
     }
 
-    private static void encrypt(String filename){
+    private static void addFile(String filename) {
+        File path = new File(database);
+        if(!Files.exists(Paths.get(database))){
+            path.mkdir();
+        }
+        encryptFile(filename);
+    }
+
+    private static void displayFile(String imageName) {
+        File path = new File(display);
+        if(!Files.exists(Paths.get(display))){
+            path.mkdir();
+        }
+        decryptFile(imageName);
+    }
+
+    private static void listFile() {
+        File path = new File(database);
+        if(!Files.exists(Paths.get(database))){
+            path.mkdir();
+        }
+        String[] files;
+        files = path.list();
+        for (String file : files){
+            //System.out.println(file.split(".")[0]);
+            System.out.println(file.substring(0, file.length()-2));
+        }
+    }
+
+    // the file are encrypted and decrypted in CBC mode
+    private static void encryptFile(String filename){
         try {
             FileInputStream fis = new FileInputStream(filename);
             byte encrypt[] = new byte[fis.available()];
             fis.read(encrypt);
-            int i = 0;
-            for (byte b : encrypt){
-                encrypt[i] = (byte) (b ^ key);
-                i++;
+            encrypt[0] = (byte) (encrypt[0] ^ IV);
+            encrypt[0] = encrypt(encrypt[0]);
+            for (int i = 1; i < encrypt.length; i++){
+                encrypt[i] = (byte) (encrypt[i] ^ encrypt[i-1]);
+                encrypt[i] = encrypt(encrypt[i]);
             }
-            File newFile = new File(filename + "en");
+            Path path = Paths.get(filename);
+            String newFileName = path.getFileName() + "en";
+            File dir = new File(database);
+
+            File newFile = new File(dir, newFileName);
             newFile.createNewFile();
             FileOutputStream fos = new FileOutputStream(newFile, false);
             fos.write(encrypt);
             fos.close();
             fis.close();
-            System.out.println("finished");
+            System.out.println("encryption finished");
         } catch (FileNotFoundException e) {
             System.out.println("The file was not found.");
             return;
@@ -43,23 +94,37 @@ public class ImageRepo {
             return;
         }
     }
-    private static void decrypt(String filename){
+    private static void decryptFile(String filename){
         try {
-            FileInputStream fis = new FileInputStream(filename);
+            Path path = Paths.get(filename);
+            String newFileName = path.getFileName() + "en";
+            File dir = new File(database);
+            File newFile = new File(dir, newFileName);
+
+            FileInputStream fis = new FileInputStream(newFile);
             byte decrypt[] = new byte[fis.available()];
             fis.read(decrypt);
-            int i = 0;
-            for (byte b : decrypt){
-                decrypt[i] = (byte) (b ^ key);
-                i++;
+
+            for (int i = decrypt.length - 1; i >= 0; i--){
+                decrypt[i] = decrypt(decrypt[i]);
+                if(i == 0){
+                    decrypt[i] = (byte) (decrypt[i] ^ IV);
+                }
+                else{
+                    decrypt[i] = (byte) (decrypt[i] ^ decrypt[i - 1]);
+                }
             }
-            File newFile = new File(filename.substring(0, filename.length()-2));
-            newFile.createNewFile();
-            FileOutputStream fos = new FileOutputStream(newFile, false);
+
+            String fileName = path.getFileName() + "";
+            File displayDir = new File(display);
+            File displayFile = new File(displayDir, fileName);
+
+            displayFile.createNewFile();
+            FileOutputStream fos = new FileOutputStream(displayFile, false);
             fos.write(decrypt);
             fos.close();
             fis.close();
-            System.out.println("finished");
+            System.out.println("decryption finished");
         } catch (FileNotFoundException e) {
             System.out.println("The file was not found.");
             return;
@@ -68,5 +133,14 @@ public class ImageRepo {
             io.printStackTrace();
             return;
         }
+    }
+
+    // just using XOR not very secure but by updating these function we can use more secured functions
+    // like AES or other block ciphers
+    private static byte encrypt (byte block){
+        return (byte)(block ^ key);
+    }
+    private static byte decrypt (byte block){
+        return (byte)(block ^ key);
     }
 }
